aa7d30fc6e88275063b9e42432c49adb8c20c158
Aleksandar Vasilic <48293920+babazorka@users.noreply.github.com>
2021-08-10 14:47:46 +0200

    radi pustanje pesme, treba iscupati trajanje

--- is1_projekat/src/main/java/endpoints/Istorija_pesama.java
+++ is1_projekat/src/main/java/endpoints/Istorija_pesama.java
@@ -11,7 +11,14 @@ import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import filters.BasicAuthFilter;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.PathParam;
 import resources.entities.Korisnik;
 import resources.entities.Pesma;
@@ -33,10 +40,35 @@ public class Istorija_pesama {
         List<Pesma> resultList = ulogovan.getPesmaList();
         return resultList;
     }
-    
+
     @Path("{videoID}")
     @GET
-    public String pustiPesmu(@PathParam("videoID") String videoID){
+    public String pustiPesmu(@PathParam("videoID") String videoID) throws InterruptedException {
+        try {
+
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        ProcessBuilder pb
+                                = new ProcessBuilder(
+                                        "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+                                        "youtube.com\\watch?v=hPQGj6G0mD8&t");
+
+                        pb.start();
+
+                        System.out.println("Google Chrome launched!");
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
+            };
+            thread.start();
+            thread.join(4000);
+        } catch (InterruptedException e) {
+            System.out.println("endpoints.Istorija_pesama.pustiPesmu()");
+        }
+
         return videoID;
     }
 
